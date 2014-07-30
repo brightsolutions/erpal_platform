4b830127e9f837798b6c487bffeb9c55770f61c7
Alex Shipilov <shipilovsasha@gmail.com>
2014-07-30 12:53:36 +0200

    fix js issue with tabs on quote and invoice view pages

--- modules/profile/erpal_commerce/modules/erpal_commerce_ui/js/erpal_commerce_ui.js
+++ modules/profile/erpal_commerce/modules/erpal_commerce_ui/js/erpal_commerce_ui.js
@@ -4,14 +4,15 @@
   Drupal.behaviors.erpalChangeTab = {
     attach: function (context, settings) {
 
-      if ($(".vbo-views-form").length == 0){
+      if ($(".view-erpal-invoice-line-items").length &&
+        $(".view-erpal-invoice-line-items .vbo-views-form").length == 0){
         var tabsID = Drupal.settings.panelsTabs.tabsID;
 
         for (var key in Drupal.settings.panelsTabs.tabsID) {
           var count = $('#' + tabsID[key] + '>ul li', context).size();
-          console.log(count);
-          $('#' + tabsID[key], context)
-            .tabs({ selected: count - 1 });
+          $('#' + tabsID[key], context).tabs({
+            selected: count - 1
+          });
         }
       }
     }
