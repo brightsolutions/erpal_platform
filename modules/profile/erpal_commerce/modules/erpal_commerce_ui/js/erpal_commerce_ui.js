c8337b894a4c719eff411f5138198a1e0b3f2290
Andrew Siz <andrew.sizzis@gmail.com>
2014-08-01 17:11:06 +0300

    Changed issue with js

--- modules/profile/erpal_commerce/modules/erpal_commerce_ui/js/erpal_commerce_ui.js
+++ modules/profile/erpal_commerce/modules/erpal_commerce_ui/js/erpal_commerce_ui.js
@@ -3,13 +3,8 @@
   // Just change default tab if bulk operation was submited.
   Drupal.behaviors.erpalChangeTab = {
     attach: function (context, settings) {
-
-<<<<<<< HEAD
-      if ($(".section-order").length > 0 && $(".vbo-views-form").length == 0){
-=======
       if ($(".view-erpal-invoice-line-items").length &&
         $(".view-erpal-invoice-line-items .vbo-views-form").length == 0){
->>>>>>> 3d47b48d83de180daf10d52a38d71e2bd9e82624
         var tabsID = Drupal.settings.panelsTabs.tabsID;
 
         for (var key in Drupal.settings.panelsTabs.tabsID) {
