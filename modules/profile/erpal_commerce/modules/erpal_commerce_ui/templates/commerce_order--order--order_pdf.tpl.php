dea49d8e4b62774d9d4f7f89c4ab55f763f0486e
Alex Shipilov <shipilovsasha@gmail.com>
2014-08-26 00:20:25 +0200

    add quote and order numbers to pdf pages

--- modules/profile/erpal_commerce/modules/erpal_commerce_ui/templates/commerce_order--order--order_pdf.tpl.php
+++ modules/profile/erpal_commerce/modules/erpal_commerce_ui/templates/commerce_order--order--order_pdf.tpl.php
@@ -20,7 +20,9 @@
   <div class="order-header-date"><?php print render($content['order_header_date']); ?></div>
   <div class="customer"><?php print render($content['commerce_customer_billing']); ?></div>
   <div class="order-number"><?php print render($content['order_number']); ?></div>
-
+  <?php if (isset($content['order_quote_number'])) : ?>
+    <div class="order-quote-number"><?php print render($content['order_quote_number']); ?></div>
+  <?php endif; ?>
   <div class="line-items">
     <div class="line-items-view"><?php print render($content['commerce_line_items']); ?></div>
     <div class="order-total"><?php print render($content['commerce_order_total']); ?></div>
