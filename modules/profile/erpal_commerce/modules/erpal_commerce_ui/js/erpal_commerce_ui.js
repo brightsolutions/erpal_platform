9518bcf04ab8e87d5752d4c2b70a588753fcdbf2
Andrew Siz <andrew.sizzis@gmail.com>
2015-01-20 12:42:12 +0200

    Improved views and fixed bugs with user interface

--- modules/profile/erpal_commerce/modules/erpal_commerce_ui/js/erpal_commerce_ui.js
+++ modules/profile/erpal_commerce/modules/erpal_commerce_ui/js/erpal_commerce_ui.js
@@ -2,12 +2,24 @@
 
   Drupal.behaviors.erpalChangeTab = {
     attach: function (context, settings) {
+
+      // Add hash of tab to form action.
       $('.vbo-views-form').each(function() {
         var action = $('form', $(this)).attr('action');
         if(action.indexOf("#") == -1) {
           var hash = $(this).closest("div[id^='tabs']").attr('id');
           if (hash) {
             $('form', $(this)).attr('action', action + '#' + hash);
+          }
+        }
+      });
+
+      $('.view-filters').each(function() {
+        var action = $('form', $(this)).attr('action');
+        if(action.indexOf("#") == -1) {
+          var hash = $(this).closest("div[id^='tabs']").attr('id');
+          if (hash) {
+            $('form', $(this)).attr('action', window.location.pathname + '#' + hash);
           }
         }
       });
